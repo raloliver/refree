0508f7657befe2c5abf261cecd200f280c63f8b8
raloliver <raloliver.ws@gmail.com>
2023-09-08 05:35:43 +0100

    feat: init header

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@
  * File: App.js
  * Project: refree
  * Created: Wednesday, September 6th 2023, 7:23:01 am
- * Last Modified: Thursday, September 7th 2023, 5:17:49 am
+ * Last Modified: Friday, September 8th 2023, 5:33:53 am
  * Copyright © 2023 AMDE Agência
  */
 
 import { Styles } from './components/GlobalStyles/Styles';
 import { Card } from './components/Card';
 import { Theme } from './components/ThemeProvider';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <Theme>
       <Styles />
+      <Header>
+        <h1>Header</h1>
+      </Header>
       <Card>
-        <h1>ReFree</h1>
+        <h3>ReFree</h3>
       </Card>
     </Theme>
   );
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+/*
+ * File: index.jsx
+ * Project: refree
+ * Created: Friday, September 8th 2023, 5:31:10 am
+ * Last Modified: Friday, September 8th 2023, 5:31:33 am
+ * Copyright © 2023 AMDE Agência
+ */
+
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  background: ${(props) => props.theme.colors.primary.a};
+  padding: ${(props) => props.theme.spacing.m};
+`;
